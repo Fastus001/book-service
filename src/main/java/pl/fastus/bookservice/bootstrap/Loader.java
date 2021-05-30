5053b64fe64445b86a1d564cf9398420a0d9049f
Fastus001 <tkarczmarzyk@gmail.com>
2021-05-30 09:39:54 +0200

    added profiles and SQL

--- src/main/java/pl/fastus/bookservice/bootstrap/Loader.java
+++ src/main/java/pl/fastus/bookservice/bootstrap/Loader.java
@@ -2,6 +2,7 @@ package pl.fastus.bookservice.bootstrap;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import pl.fastus.bookservice.domain.Author;
 import pl.fastus.bookservice.domain.Book;
@@ -13,6 +14,7 @@ import pl.fastus.bookservice.services.BookService;
  */
 @RequiredArgsConstructor
 @Component
+@Profile("!prod")
 public class Loader implements CommandLineRunner {
 
     private final BookService bookService;
